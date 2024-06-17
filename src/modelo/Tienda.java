5fc57fa27203cb57a2f357224488f93be23e5c81
David_Puetate <david.puetate617@ist17dejulio.edu.ec>
2024-06-17 14:32:25 -0500

    Creacion de la clase tienda

---
+++ src/modelo/Tienda.java
@@ -0,0 +1,10 @@
+package modelo;
+/**
+ *
+ * @author david
+ */
+public class Tienda {
+    private String dfpcNombreTienda;
+    private String dfpcNombrePropietario;
+    private int dfpcIdentificadorTributario;
+}
